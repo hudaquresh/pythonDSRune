6535346b1ae7a687861b23ec890c150b65257a0e
hudaquresh <hudaquresh@gmail.com>
2015-12-31 16:40:42 -0800

    modified listing_1.py

--- sortingAndSearching/hashing/listing_1.py
+++ sortingAndSearching/hashing/listing_1.py
@@ -14,7 +14,23 @@ def hash(aString, tableSize):
 	return sum%tableSize
 
 
+def hashModified(aString, tableSize): 
+	# A modified version of the hash function to account for anagrams
+	
+	sum = 0 
+	for pos in range(len(aString)):
+		sum = sum + ord(aString[pos])*(pos + 1)
+	return sum%tableSize 
+
 if __name__ == '__main__': 
 	
-	# Test a few values 
-	Test
+	#TODO  Practice writing test cases 
+	#TODO  Test more interesting examples  
+	var1 = hash('cat', 11)
+	print var1
+	assert var1 == 4 
+
+	var2 = hashModified('cat', 11)
+	print var2
+	assert var2 == 3
+
